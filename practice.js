07304ab3d4df01b60c154a0ea0fd484be1d349f0
Derek Scott <dered806@gmail.com>
2019-07-24 21:00:29 -0600

    completed probs 1 - 4, working on 5

--- practice.js
+++ practice.js
@@ -67,8 +67,9 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 
 //Code Here
 function reversedLooper(letters){
-  for (let i =0; i = letters.length - 1; i >=0; i--){}
-window.alert(letters[i])
+for (let i = letters.length - 1; i >= 0; i--) {
+  window.alert(letters[i])
+}
 }
 
 
@@ -85,9 +86,14 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 */
 
 //
-// function evenFinder(nums){
-// for (var i = 0; i < nums.length; i++)
-// }
+var evenNums =[]
+function evenFinder(nums){
+for (var i = 0; i < nums.length; i++){
+  if (nums[i] % 2 === 0) {
+  evenNums.push(nums[i]);
+  }
+  }
+}
 
 
 
